460b47ea317e1fedb96c3d318e14afbcbc60cca1
Arda Gökçek <ardagokcek@hotmail.com>
2018-11-15 20:37:35 +0100

    Uploaded Crowdsale and renamed Token file.

---
+++ Crowdsale.sol
@@ -0,0 +1,615 @@
+pragma solidity ^0.4.24;
+
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, reverts on overflow.
+  */
+  function mul(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (_a == 0) {
+      return 0;
+    }
+
+    uint256 c = _a * _b;
+    require(c / _a == _b);
+
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
+  */
+  function div(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    require(_b > 0); // Solidity only automatically asserts when dividing by 0
+    uint256 c = _a / _b;
+    // assert(_a == _b * c + _a % _b); // There is no case in which this doesn't hold
+
+    return c;
+  }
+
+  /**
+  * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    require(_b <= _a);
+    uint256 c = _a - _b;
+
+    return c;
+  }
+
+  /**
+  * @dev Adds two numbers, reverts on overflow.
+  */
+  function add(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    uint256 c = _a + _b;
+    require(c >= _a);
+
+    return c;
+  }
+
+  /**
+  * @dev Divides two numbers and returns the remainder (unsigned integer modulo),
+  * reverts when dividing by zero.
+  */
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b != 0);
+    return a % b;
+  }
+}
+library SafeERC20 {
+  function safeTransfer(
+    IERC20 token,
+    address to,
+    uint256 value
+  )
+    internal
+  {
+    require(token.transfer(to, value));
+  }
+
+  function safeTransferFrom(
+    IERC20 token,
+    address from,
+    address to,
+    uint256 value
+  )
+    internal
+  {
+    require(token.transferFrom(from, to, value));
+  }
+
+  function safeApprove(
+    IERC20 token,
+    address spender,
+    uint256 value
+  )
+    internal
+  {
+    require(token.approve(spender, value));
+  }
+}
+library Roles {
+  struct Role {
+    mapping (address => bool) bearer;
+  }
+
+  /**
+   * @dev give an account access to this role
+   */
+  function add(Role storage role, address account) internal {
+    require(account != address(0));
+    role.bearer[account] = true;
+  }
+
+  /**
+   * @dev remove an account's access to this role
+   */
+  function remove(Role storage role, address account) internal {
+    require(account != address(0));
+    role.bearer[account] = false;
+  }
+
+  /**
+   * @dev check if an account has this role
+   * @return bool
+   */
+  function has(Role storage role, address account)
+    internal
+    view
+    returns (bool)
+  {
+    require(account != address(0));
+    return role.bearer[account];
+  }
+}
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+interface IERC20 {
+  function totalSupply() external view returns (uint256);
+
+  function balanceOf(address who) external view returns (uint256);
+
+  function allowance(address owner, address spender)
+    external view returns (uint256);
+
+  function transfer(address to, uint256 value) external returns (bool);
+
+  function approve(address spender, uint256 value)
+    external returns (bool);
+
+  function transferFrom(address from, address to, uint256 value)
+    external returns (bool);
+
+  event Transfer(
+    address indexed from,
+    address indexed to,
+    uint256 value
+  );
+
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
+  
+  event Freeze(
+      address indexed account,
+      uint256 indexed value,
+      uint8 indexed place);
+}
+
+  /**
+   * @dev This contract has been modified by Around App Co-Founder Arda Gokcek
+   * to make the owner variable an address list.
+   */
+contract Ownable {
+    mapping (address => bool) owners;
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+  event OwnershipGiven(address indexed _from, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  constructor() public {
+    owners[msg.sender] = true;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(owners[msg.sender]);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function giveOwnership(address _newOwner) public onlyOwner {
+    require(_newOwner != address(0));
+    owners[_newOwner] = true;
+    emit OwnershipGiven(msg.sender, _newOwner);
+  }
+  
+  function takeOwnership(address _owner) public onlyOwner {
+      owners[_owner] = false;
+  }
+}
+contract Whitelist is Ownable {
+    using Roles for Roles.Role;
+    Roles.Role private contributors;
+    
+    uint256 private whitelistLimit = 0; 
+    
+    modifier onlyWhitelisted() {
+    require(isWhitelisted(msg.sender));
+    _;
+    }
+    
+    modifier onlyinLimits() {
+        require(whitelistLimit<=20000);
+        _;
+    }
+    
+    function isWhitelisted(address account) public view returns (bool) {
+        return contributors.has(account);
+    }
+    
+    function addUser(address account) public onlyOwner onlyinLimits {
+        contributors.add(account);
+        whitelistLimit++;
+    }
+    
+    function addUsers(address[] _accounts) public onlyOwner onlyinLimits{
+        for (uint8 i = 0; i < _accounts.length; i++) {
+            addUser(_accounts[i]);
+        }
+        whitelistLimit = whitelistLimit + _accounts.length;
+    }
+    function kickUser(address account) public onlyOwner {
+        contributors.remove(account);
+    }
+    
+    function kickUsers(address[] _accounts) public onlyOwner {
+        for (uint8 i = 0; i < _accounts.length; i++) {
+            kickUser(_accounts[i]);
+        }
+    }
+}
+contract Crowdsale is Whitelist {
+  using SafeMath for uint256;
+  using SafeERC20 for IERC20;
+
+  // The token being sold
+  IERC20 private _token;
+
+  // Address where funds are collected
+  address private _wallet;
+
+  // How many token units a buyer gets per wei.
+  // The rate is the conversion between wei and the smallest and indivisible token unit.
+  // So, if you are using a rate of 1 with a ERC20Detailed token with 3 decimals called TOK
+  // 1 wei will give you 1 unit, or 0.001 TOK.
+  uint256 private _rate;
+
+  // Amount of wei raised
+  uint256 private _weiRaised;
+  
+  // AROUND APP EXTENSION Personal Contribution Data
+  mapping (address => uint256) public contribution;
+
+  /**
+   * Event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+  event TokensPurchased(
+    address indexed purchaser,
+    address indexed beneficiary,
+    uint256 value,
+    uint256 amount
+  );
+
+  /**
+   * @param rate Number of token units a buyer gets per wei
+   * @dev The rate is the conversion between wei and the smallest and indivisible
+   * token unit. So, if you are using a rate of 1 with a ERC20Detailed token
+   * with 3 decimals called TOK, 1 wei will give you 1 unit, or 0.001 TOK.
+   * @param wallet Address where collected funds will be forwarded to
+   * @param token Address of the token being sold
+   */
+  constructor(uint256 rate, address wallet, IERC20 token) public {
+    require(rate > 0);
+    require(wallet != address(0));
+    require(token != address(0));
+
+    _rate = rate;
+    _wallet = wallet;
+    _token = token;
+  }
+
+  // -----------------------------------------
+  // Crowdsale external interface
+  // -----------------------------------------
+
+  /**
+   * @dev fallback function ***DO NOT OVERRIDE***
+   */
+  function () external payable {
+    buyTokens(msg.sender);
+  }
+
+  /**
+   * @return the token being sold.
+   */
+  function token() public view returns(IERC20) {
+    return _token;
+  }
+
+  /**
+   * @return the address where funds are collected.
+   */
+  function wallet() public view returns(address) {
+    return _wallet;
+  }
+
+  /**
+   * @return the number of token units a buyer gets per wei.
+   */
+  function rate() public view returns(uint256) {
+    return _rate;
+  }
+
+  /**
+   * @return the mount of wei raised.
+   */
+  function weiRaised() public view returns (uint256) {
+    return _weiRaised;
+  }
+
+  /**
+   * @dev low level token purchase ***DO NOT OVERRIDE***
+   * @param beneficiary Address performing the token purchase
+   */
+  function buyTokens(address beneficiary) public onlyWhitelisted payable {
+
+    uint256 weiAmount = msg.value;
+    _preValidatePurchase(beneficiary, weiAmount);
+
+    // calculate token amount to be created
+    uint256 tokens = _getTokenAmount(weiAmount);
+
+    // update state
+    _weiRaised = _weiRaised.add(weiAmount);
+    contribution[beneficiary] = contribution[beneficiary].add(weiAmount);
+    
+    _processPurchase(beneficiary, tokens);
+    emit TokensPurchased(
+      msg.sender,
+      beneficiary,
+      weiAmount,
+      tokens
+    );
+
+    _updatePurchasingState(beneficiary, weiAmount);
+
+    _forwardFunds();
+    _postValidatePurchase(beneficiary, weiAmount);
+  }
+
+  // -----------------------------------------
+  // Internal interface (extensible)
+  // -----------------------------------------
+
+  /**
+   * @dev Validation of an incoming purchase. Use require statements to revert state when conditions are not met. Use `super` in contracts that inherit from Crowdsale to extend their validations.
+   * Example from CappedCrowdsale.sol's _preValidatePurchase method:
+   *   super._preValidatePurchase(beneficiary, weiAmount);
+   *   require(weiRaised().add(weiAmount) <= cap);
+   * @param beneficiary Address performing the token purchase
+   * @param weiAmount Value in wei involved in the purchase
+   */
+  function _preValidatePurchase(
+    address beneficiary,
+    uint256 weiAmount
+  )
+    internal
+  {
+    require(beneficiary != address(0));
+    require(weiAmount != 0);
+  }
+
+  /**
+   * @dev Validation of an executed purchase. Observe state and use revert statements to undo rollback when valid conditions are not met.
+   * @param beneficiary Address performing the token purchase
+   * @param weiAmount Value in wei involved in the purchase
+   */
+  function _postValidatePurchase(
+    address beneficiary,
+    uint256 weiAmount
+  )
+    internal
+  {
+    // optional override
+  }
+
+  /**
+   * @dev Source of tokens. Override this method to modify the way in which the crowdsale ultimately gets and sends its tokens.
+   * @param beneficiary Address performing the token purchase
+   * @param tokenAmount Number of tokens to be emitted
+   */
+  function _deliverTokens(
+    address beneficiary,
+    uint256 tokenAmount
+  )
+    internal
+  {
+    _token.safeTransfer(beneficiary, tokenAmount);
+  }
+
+  /**
+   * @dev Executed when a purchase has been validated and is ready to be executed. Not necessarily emits/sends tokens.
+   * @param beneficiary Address receiving the tokens
+   * @param tokenAmount Number of tokens to be purchased
+   */
+  function _processPurchase(
+    address beneficiary,
+    uint256 tokenAmount
+  )
+    internal
+  {
+    _deliverTokens(beneficiary, tokenAmount);
+  }
+
+  /**
+   * @dev Override for extensions that require an internal state to check for validity (current user contributions, etc.)
+   * @param beneficiary Address receiving the tokens
+   * @param weiAmount Value in wei involved in the purchase
+   */
+  function _updatePurchasingState(
+    address beneficiary,
+    uint256 weiAmount
+  )
+    internal
+  {
+    // optional override
+  }
+
+  /**
+   * @dev Override to extend the way in which ether is converted to tokens.
+   * @param weiAmount Value in wei to be converted into tokens
+   * @return Number of tokens that can be purchased with the specified _weiAmount
+   */
+  function _getTokenAmount(uint256 weiAmount)
+    internal view returns (uint256)
+  {
+    return weiAmount.mul(_rate);
+  }
+
+  /**
+   * @dev Determines how ETH is stored/forwarded on purchases.
+   */
+  function _forwardFunds() internal {
+    _wallet.transfer(msg.value);
+  }
+}
+contract TimedCrowdsale is Crowdsale {
+  using SafeMath for uint256;
+
+  uint256 private _openingTime;
+  uint256 private _closingTime;
+
+  /**
+   * @dev Reverts if not in crowdsale time range.
+   */
+  modifier onlyWhileOpen {
+    require(isOpen());
+    _;
+  }
+
+  /**
+   * @dev Constructor, takes crowdsale opening and closing times.
+   * @param openingTime Crowdsale opening time
+   * @param closingTime Crowdsale closing time
+   */
+  constructor(uint256 openingTime, uint256 closingTime) public {
+    // solium-disable-next-line security/no-block-members
+    require(openingTime >= block.timestamp);
+    require(closingTime >= openingTime);
+
+    _openingTime = openingTime;
+    _closingTime = closingTime;
+  }
+
+  /**
+   * @return the crowdsale opening time.
+   */
+  function openingTime() public view returns(uint256) {
+    return _openingTime;
+  }
+
+  /**
+   * @return the crowdsale closing time.
+   */
+  function closingTime() public view returns(uint256) {
+    return _closingTime;
+  }
+
+  /**
+   * @return true if the crowdsale is open, false otherwise.
+   */
+  function isOpen() public view returns (bool) {
+    // solium-disable-next-line security/no-block-members
+    return block.timestamp >= _openingTime && block.timestamp <= _closingTime;
+  }
+
+  /**
+   * @dev Checks whether the period in which the crowdsale is open has already elapsed.
+   * @return Whether crowdsale period has elapsed
+   */
+  function hasClosed() public view returns (bool) {
+    // solium-disable-next-line security/no-block-members
+    return block.timestamp > _closingTime;
+  }
+
+  /**
+   * @dev Extend parent behavior requiring to be within contributing period
+   * @param beneficiary Token purchaser
+   * @param weiAmount Amount of wei contributed
+   */
+  function _preValidatePurchase(
+    address beneficiary,
+    uint256 weiAmount
+  )
+    internal
+    onlyWhileOpen
+  {
+    super._preValidatePurchase(beneficiary, weiAmount);
+  }
+
+}
+contract CappedCrowdsale is Crowdsale {
+  using SafeMath for uint256;
+
+  uint256 private _cap;
+  uint256 private _personalCap;
+  uint256 private _startTime;
+  /**
+   * @dev Constructor, takes maximum amount of wei accepted in the crowdsale.
+   * @param cap Max amount of wei to be contributed
+   */
+  constructor(uint256 cap, uint256 personalCap, uint256 openingTime) public {
+    require(cap > 0);
+    _cap = cap*1e18;
+    _personalCap = personalCap*1e18;
+    _startTime = openingTime;
+  }
+
+  /**
+   * @return the cap of the crowdsale.
+   */
+  function cap() public view returns(uint256) {
+    return _cap;
+  }
+
+  /**
+   * @dev Checks whether the cap has been reached.
+   * @return Whether the cap was reached
+   */
+  function capReached() public view returns (bool) {
+    return weiRaised() >= _cap;
+  }
+
+  function personalCap() public view returns(uint256) {
+      return _personalCap;
+  }
+  /**
+   * @dev Extend parent behavior requiring purchase to respect the funding cap.
+   * @param beneficiary Token purchaser
+   * @param weiAmount Amount of wei contributed
+   */
+  function _preValidatePurchase(
+    address beneficiary,
+    uint256 weiAmount
+  )
+    internal
+  {
+    super._preValidatePurchase(beneficiary, weiAmount);
+    require(weiRaised().add(weiAmount) <= _cap);
+    /* AROUND EXTENSION Addition for Personal Caps */
+    if(block.timestamp < _startTime.add(86400)) {
+        require(contribution[beneficiary].add(weiAmount) <= _personalCap);
+    }
+    else {
+        require(contribution[beneficiary].add(weiAmount) <= _personalCap.mul(2));
+    }
+    }
+
+  function _postValidatePurchase(address beneficiary, uint256 weiAmount) internal {
+      contribution[beneficiary].add(weiAmount);
+  }
+
+}
+contract AroundCrowdsale is TimedCrowdsale, CappedCrowdsale {
+    constructor(uint256 openingTime, uint256 closingTime, uint256 rate, address wallet, uint256 cap, uint256 personalCap, IERC20 token) public
+        Crowdsale(rate, wallet, token)
+        CappedCrowdsale(cap, personalCap, openingTime)
+        TimedCrowdsale(openingTime, closingTime)
+        {
+            cap > 0;
+            personalCap > 0;
+        }
+}
